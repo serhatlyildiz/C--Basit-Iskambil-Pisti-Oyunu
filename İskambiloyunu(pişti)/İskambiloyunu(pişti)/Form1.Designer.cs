47fbe0c09e687c718bfbca5c92513608240888e7
Serhat <115489292+serhatlyildiz@users.noreply.github.com>
2024-06-29 07:16:51 +0300

    C# ile Basit Iskambil Oyunu
    2 Kişilik Pişti Oyunu

---
+++ İskambiloyunu(pişti)/İskambiloyunu(pişti)/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace İskambiloyunu_pişti_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.yerkarti = new System.Windows.Forms.PictureBox();
+            this.pct_oyuncu2 = new System.Windows.Forms.PictureBox();
+            this.pct_oyuncu1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btn_oyuncu1 = new System.Windows.Forms.Button();
+            this.btn_oyuncu2 = new System.Windows.Forms.Button();
+            this.pct_yeroyuncu1 = new System.Windows.Forms.PictureBox();
+            this.pct_yeroyuncu2 = new System.Windows.Forms.PictureBox();
+            this.lbl_oyuncu1 = new System.Windows.Forms.Label();
+            this.lbl_oyuncu2 = new System.Windows.Forms.Label();
+            this.puan1 = new System.Windows.Forms.Label();
+            this.puan2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.yerkarti)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct_oyuncu2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct_oyuncu1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct_yeroyuncu1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct_yeroyuncu2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // yerkarti
+            // 
+            this.yerkarti.Location = new System.Drawing.Point(309, 77);
+            this.yerkarti.Name = "yerkarti";
+            this.yerkarti.Size = new System.Drawing.Size(140, 177);
+            this.yerkarti.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.yerkarti.TabIndex = 0;
+            this.yerkarti.TabStop = false;
+            // 
+            // pct_oyuncu2
+            // 
+            this.pct_oyuncu2.Location = new System.Drawing.Point(620, 77);
+            this.pct_oyuncu2.Name = "pct_oyuncu2";
+            this.pct_oyuncu2.Size = new System.Drawing.Size(134, 177);
+            this.pct_oyuncu2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pct_oyuncu2.TabIndex = 1;
+            this.pct_oyuncu2.TabStop = false;
+            // 
+            // pct_oyuncu1
+            // 
+            this.pct_oyuncu1.Location = new System.Drawing.Point(11, 77);
+            this.pct_oyuncu1.Name = "pct_oyuncu1";
+            this.pct_oyuncu1.Size = new System.Drawing.Size(134, 177);
+            this.pct_oyuncu1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pct_oyuncu1.TabIndex = 2;
+            this.pct_oyuncu1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "1.OYUNCU";
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(667, 33);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(87, 17);
+            this.label.TabIndex = 4;
+            this.label.Text = "2.OYUNCU";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(346, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "DAĞIT";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_oyuncu1
+            // 
+            this.btn_oyuncu1.BackColor = System.Drawing.Color.White;
+            this.btn_oyuncu1.Location = new System.Drawing.Point(40, 151);
+            this.btn_oyuncu1.Name = "btn_oyuncu1";
+            this.btn_oyuncu1.Size = new System.Drawing.Size(75, 23);
+            this.btn_oyuncu1.TabIndex = 6;
+            this.btn_oyuncu1.Text = "ÇEK";
+            this.btn_oyuncu1.UseVisualStyleBackColor = false;
+            this.btn_oyuncu1.Visible = false;
+            this.btn_oyuncu1.Click += new System.EventHandler(this.btn_oyuncu1_Click);
+            // 
+            // btn_oyuncu2
+            // 
+            this.btn_oyuncu2.BackColor = System.Drawing.Color.White;
+            this.btn_oyuncu2.Location = new System.Drawing.Point(650, 151);
+            this.btn_oyuncu2.Name = "btn_oyuncu2";
+            this.btn_oyuncu2.Size = new System.Drawing.Size(75, 23);
+            this.btn_oyuncu2.TabIndex = 7;
+            this.btn_oyuncu2.Text = "ÇEK";
+            this.btn_oyuncu2.UseVisualStyleBackColor = false;
+            this.btn_oyuncu2.Visible = false;
+            this.btn_oyuncu2.Click += new System.EventHandler(this.btn_oyuncu2_Click);
+            // 
+            // pct_yeroyuncu1
+            // 
+            this.pct_yeroyuncu1.Location = new System.Drawing.Point(151, 100);
+            this.pct_yeroyuncu1.Name = "pct_yeroyuncu1";
+            this.pct_yeroyuncu1.Size = new System.Drawing.Size(100, 138);
+            this.pct_yeroyuncu1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pct_yeroyuncu1.TabIndex = 8;
+            this.pct_yeroyuncu1.TabStop = false;
+            this.pct_yeroyuncu1.Visible = false;
+            // 
+            // pct_yeroyuncu2
+            // 
+            this.pct_yeroyuncu2.Location = new System.Drawing.Point(516, 100);
+            this.pct_yeroyuncu2.Name = "pct_yeroyuncu2";
+            this.pct_yeroyuncu2.Size = new System.Drawing.Size(100, 138);
+            this.pct_yeroyuncu2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pct_yeroyuncu2.TabIndex = 9;
+            this.pct_yeroyuncu2.TabStop = false;
+            this.pct_yeroyuncu2.Visible = false;
+            // 
+            // lbl_oyuncu1
+            // 
+            this.lbl_oyuncu1.AutoSize = true;
+            this.lbl_oyuncu1.Location = new System.Drawing.Point(306, 285);
+            this.lbl_oyuncu1.Name = "lbl_oyuncu1";
+            this.lbl_oyuncu1.Size = new System.Drawing.Size(142, 17);
+            this.lbl_oyuncu1.TabIndex = 10;
+            this.lbl_oyuncu1.Text = "1.Oyuncu Puanı = ";
+            this.lbl_oyuncu1.Visible = false;
+            // 
+            // lbl_oyuncu2
+            // 
+            this.lbl_oyuncu2.AutoSize = true;
+            this.lbl_oyuncu2.Location = new System.Drawing.Point(306, 340);
+            this.lbl_oyuncu2.Name = "lbl_oyuncu2";
+            this.lbl_oyuncu2.Size = new System.Drawing.Size(142, 17);
+            this.lbl_oyuncu2.TabIndex = 11;
+            this.lbl_oyuncu2.Text = "2.Oyuncu Puanı = ";
+            this.lbl_oyuncu2.Visible = false;
+            // 
+            // puan1
+            // 
+            this.puan1.AutoSize = true;
+            this.puan1.Location = new System.Drawing.Point(454, 285);
+            this.puan1.Name = "puan1";
+            this.puan1.Size = new System.Drawing.Size(0, 17);
+            this.puan1.TabIndex = 12;
+            this.puan1.Visible = false;
+            // 
+            // puan2
+            // 
+            this.puan2.AutoSize = true;
+            this.puan2.Location = new System.Drawing.Point(454, 340);
+            this.puan2.Name = "puan2";
+            this.puan2.Size = new System.Drawing.Size(0, 17);
+            this.puan2.TabIndex = 13;
+            this.puan2.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Maroon;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(638, 297);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 60);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "YENİDEN BAŞLA";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.ForestGreen;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(768, 392);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.puan2);
+            this.Controls.Add(this.puan1);
+            this.Controls.Add(this.lbl_oyuncu2);
+            this.Controls.Add(this.lbl_oyuncu1);
+            this.Controls.Add(this.pct_yeroyuncu2);
+            this.Controls.Add(this.pct_yeroyuncu1);
+            this.Controls.Add(this.btn_oyuncu2);
+            this.Controls.Add(this.btn_oyuncu1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pct_oyuncu1);
+            this.Controls.Add(this.pct_oyuncu2);
+            this.Controls.Add(this.yerkarti);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "İSKAMBİL OYUNU";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.yerkarti)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct_oyuncu2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct_oyuncu1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct_yeroyuncu1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct_yeroyuncu2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox yerkarti;
+        private System.Windows.Forms.PictureBox pct_oyuncu2;
+        private System.Windows.Forms.PictureBox pct_oyuncu1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_oyuncu1;
+        private System.Windows.Forms.Button btn_oyuncu2;
+        private System.Windows.Forms.PictureBox pct_yeroyuncu1;
+        private System.Windows.Forms.PictureBox pct_yeroyuncu2;
+        private System.Windows.Forms.Label lbl_oyuncu1;
+        private System.Windows.Forms.Label lbl_oyuncu2;
+        private System.Windows.Forms.Label puan1;
+        private System.Windows.Forms.Label puan2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ İskambiloyunu(pişti)/İskambiloyunu(pişti)/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace İskambiloyunu_pişti_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void metod()
+        {
+            if (yerkartisonhane == 's')
+            {
+                puan += 11;
+            }
+            else if (yerkartisonhane == 'x' || yerkartisonhane == 'z')
+            {
+                puan += 10;
+            }
+            else if (yerkartisonhane == 'r')
+            {
+                puan += 25;
+            }
+            else
+            {
+                puan += Convert.ToInt16(yerkartisonhane);
+            }
+        }
+        OleDbConnection cnt_baglanti;
+        List<string> kartlar = new List<string>();
+        List<string> oyuncu1kartlar = new List<string>();
+        List<string> oyuncu2kartlar = new List<string>();
+        char yerkartisonhane;
+        int puan;
+        int puan1oyuncu=0;
+        int puan2oyuncu=0;
+        Random rnd = new Random();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cnt_baglanti = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=kartlar.accdb");
+            cnt_baglanti.Open();
+            OleDbDataAdapter adp_dagit = new OleDbDataAdapter("select * from kartlar",cnt_baglanti);
+            DataTable dtp_dagit = new DataTable();
+            adp_dagit.Fill(dtp_dagit);
+            do
+            {
+                int rastgele = rnd.Next(0,52);
+                if (kartlar.IndexOf(dtp_dagit.Rows[rastgele]["kartadi"].ToString()) == -1)
+                {
+                    kartlar.Add(dtp_dagit.Rows[rastgele]["kartadi"].ToString());
+                }
+            } while (kartlar.Count!=52);
+            for (int i = 0; i < 26; i++)
+            {
+                oyuncu1kartlar.Add(kartlar[i]);
+            }
+            for (int a = 26; a < 52; a++)
+            {
+                oyuncu2kartlar.Add(kartlar[a]);
+            }
+            button1.Visible = false;
+            puan1.Visible = true;
+            puan2.Visible = true;
+            lbl_oyuncu1.Visible = true;
+            lbl_oyuncu2.Visible = true;
+            pct_yeroyuncu1.Visible = true;
+            pct_yeroyuncu2.Visible = true;
+            pct_yeroyuncu2.BackColor = Color.ForestGreen;
+            pct_yeroyuncu1.BackColor = Color.ForestGreen;
+            pct_oyuncu1.ImageLocation = "arka.jpg";
+            pct_oyuncu2.ImageLocation = "arka.jpg";
+            yerkarti.ImageLocation = "";
+            yerkarti.BackColor = Color.ForestGreen;
+            btn_oyuncu1.Visible = true;
+            btn_oyuncu2.Visible = true;
+            btn_oyuncu2.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            yerkarti.ImageLocation ="arka.jpg";
+        }
+
+        private void btn_oyuncu1_Click(object sender, EventArgs e)
+        {
+            if (yerkarti.ImageLocation=="")
+            {
+                puan = 0;
+                yerkarti.ImageLocation = "iskambil_Ödev//" + oyuncu1kartlar[0] + ".png";
+                yerkartisonhane = oyuncu1kartlar[0][oyuncu1kartlar[0].Length - 1];
+                metod();
+            }
+            else if (yerkartisonhane == oyuncu1kartlar[0][oyuncu1kartlar[0].Length-1])
+            {
+                metod();
+                pct_yeroyuncu2.ImageLocation = "iskambil_Ödev//" + oyuncu1kartlar[0] + ".png";
+                yerkarti.ImageLocation = "";
+                puan2oyuncu += puan;
+            }
+            else
+            {
+                yerkarti.ImageLocation = "iskambil_Ödev//" + oyuncu1kartlar[0] + ".png";
+                yerkartisonhane = oyuncu1kartlar[0][oyuncu1kartlar[0].Length-1];
+            }
+            oyuncu1kartlar.RemoveAt(0);
+            btn_oyuncu2.Enabled = true;
+            btn_oyuncu1.Enabled = false;
+            if (oyuncu1kartlar.Count==0)
+            {
+                pct_oyuncu1.ImageLocation = "";
+                pct_oyuncu1.BackColor = Color.ForestGreen;
+            }
+        }
+
+        private void btn_oyuncu2_Click(object sender, EventArgs e)
+        {
+            if (yerkarti.ImageLocation == "")
+            {
+                puan = 0;
+                yerkarti.ImageLocation = "iskambil_Ödev//" + oyuncu2kartlar[0] + ".png";
+                yerkartisonhane = oyuncu2kartlar[0][oyuncu2kartlar[0].Length - 1];
+                metod();
+            }
+            else  if (yerkartisonhane == oyuncu2kartlar[0][oyuncu2kartlar[0].Length-1])
+            {
+                metod();
+                pct_yeroyuncu1.ImageLocation = "iskambil_Ödev//" + oyuncu2kartlar[0] + ".png";
+                yerkarti.ImageLocation = "";
+                puan1oyuncu += puan;    
+            }
+            else
+            {
+                yerkarti.ImageLocation = "iskambil_Ödev//" + oyuncu2kartlar[0] + ".png";
+                yerkartisonhane = oyuncu2kartlar[0][oyuncu2kartlar[0].Length-1];
+            }
+            oyuncu2kartlar.RemoveAt(0);
+            btn_oyuncu2.Enabled = false;
+            btn_oyuncu1.Enabled = true;
+            if (oyuncu2kartlar.Count==0)
+            {
+                btn_oyuncu1.Enabled = false;
+                btn_oyuncu2.Enabled = false;
+                pct_oyuncu2.ImageLocation = "";
+                pct_oyuncu2.BackColor = Color.ForestGreen;
+                puan1.Text = puan1oyuncu.ToString();
+                puan2.Text = puan2oyuncu.ToString();
+                if (puan1oyuncu<puan2oyuncu)
+                {
+                    MessageBox.Show("1.OYUNCU KAZANDI TEBRİKLER");
+                }
+                else if (puan1oyuncu > puan2oyuncu)
+                {
+                    MessageBox.Show("2.OYUNCU KAZANDI TEBRİKLER");
+                }
+                else
+                {
+                    MessageBox.Show("DOSTLUK KAZANDI BERABERE :)");
+                }
+            }    
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+    }
+}
